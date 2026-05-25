bc0d948dc0208f1ee36ad9d16499b26b1080178f
nemnem202 <naim.elhabbas@gmail.com>
2026-05-25 17:47:24 +0200

    feat: added new actions to control config

--- types/actions.ts
+++ types/actions.ts
@@ -19,9 +19,15 @@ export enum Action {
   REMOVE_SELECTED_NOTES,
   ADD_NOTES,
 
-  SET_BPM,
   SET_SIGNATURE,
   SET_SUBDIVISION,
+
+  SET_BPM,
+  SET_BPM_PRACTICE,
+  SET_TRANSPOSITION,
+  SET_TRANSPOSITION_PRACTICE,
+  SET_REPEATS,
+  SET_COUNT_INT,
 
   SET_TRANSPORT_START,
   SET_TRANSPORT_START_FROM_MEASURE_INDEX,
@@ -64,7 +70,6 @@ export type MidiAction = (
   | { type: Action.SELECT_NOTE; trackId: TrackId; index: NoteIndex }
   | { type: Action.SELECT_NOTES; trackId: TrackId; indexes: NoteIndex[] }
   | { type: Action.UNSELECT_ALL_NOTES; trackId: TrackId }
-  | { type: Action.SET_BPM; bpm: number }
   | { type: Action.SET_SIGNATURE; signature: [number, number] }
   | { type: Action.SET_SUBDIVISION; subdivision: [number, number] }
   | { type: Action.SET_TRANSPORT_STATUS; status: TransportStatus }
@@ -81,4 +86,10 @@ export type MidiAction = (
   | { type: Action.UNMUTE_TRACK; trackId: TrackId }
   | { type: Action.SET_TRANSPORT_START_FROM_MEASURE_INDEX; measureIndex: number }
   | { type: Action.ZoomY; zoomY: number }
+  | { type: Action.SET_BPM; bpm: number }
+  | { type: Action.SET_BPM_PRACTICE; bpm: number }
+  | { type: Action.SET_TRANSPOSITION; transposition: number }
+  | { type: Action.SET_TRANSPOSITION_PRACTICE; transposition: number }
+  | { type: Action.SET_REPEATS; repeats: number }
+  | { type: Action.SET_COUNT_INT; countin: boolean }
 ) & { skipHistory?: boolean };
